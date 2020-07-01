68b50152eafb9c5ad61175ccfae7d2ab1c2d0896
Niels Koster <n.koster@portavita.eu>
2020-07-02 01:04:33 +0200

    signin / signout stuff

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { Header } from './src/components/common'
+import { Header, Button } from './src/components/common'
 import LoginForm from './src/components/LoginForm'
+import firebase from 'firebase'
+import fireCode from './firecode'
 
 class App extends Component {
+  state = { signedIn: false }
+  componentDidMount() {
+    firebase.initializeApp(fireCode)
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ signedIn: true })
+      } else {
+        this.setState({ signedIn: false })
+      }
+    })
+  }
+  renderForm() {
+    if (this.state.signedIn) {
+      return (
+        <Button onPress={_ => this.setState({ signedIn: false })}>Sign Out</Button>
+      )
+    } else {
+      return (
+        <LoginForm firebase={firebase}/>
+      )
+    }
+  }
   render() {
     return (
       <View>
         <Header title='US Army Authentication' />
-        <LoginForm />
+        {this.renderForm()}
       </View>
     )
   }
